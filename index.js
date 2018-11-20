9b70b5d771d86659ab3abab1ec4986e638aa6690
Fei Qi <feiqi9047@gmail.com>
2018-11-20 17:28:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var gameInterval = null
 function checkCollision(rock) {
   const top = positionToInteger(rock.style.top)
 
-  if (top > 360) {
+  if (top <= 360) {
     const dodgerLeftEdge = positionToInteger(DODGER.style.left)
     const dodgerRightEdge = dodgerLeftEdge+40;
 
